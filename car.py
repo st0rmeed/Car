f5a127adebfe71a0f9c1c9504a730041c71292f1
st0rmed <gleblazarev967@gmail.com>
2024-12-26 23:23:55 +0300

    second

--- car.py
+++ car.py
@@ -1,28 +1,45 @@
 import pygame
 import os
-import sys
 
 pygame.init()
+
 screen = pygame.display.set_mode((600, 95))
 
-car = pygame.image.load(os.path.join('data', 'car2.png'))
-car = pygame.transform.flip(car, True, False)
 
-x = 0
-speed = -10
+class Car(pygame.sprite.Sprite):
+    def __init__(self, image_path):
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 0, 0
+        self.speed = -10
 
-while True:
-    pygame.time.delay(30)
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.left <= -10 or self.rect.right >= 600:
+            self.speed = -self.speed
+            self.image = pygame.transform.flip(self.image, True, False)
+
+
+all_sprites = pygame.sprite.Group()
+
+car_image_path = os.path.join('data', 'car2.png')
+car = Car(car_image_path)
+all_sprites.add(car)
+
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            running = False
 
-    if x >= 450 or x <= 0:
-        speed = -speed
-        car = pygame.transform.flip(car, True, False)
-
-    x += speed
-
+    all_sprites.update()
     screen.fill('white')
-    screen.blit(car, (x, 0))
+    all_sprites.draw(screen)
     pygame.display.flip()
+
+pygame.quit()
